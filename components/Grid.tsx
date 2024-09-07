189952640bdd539ae77d51f5a29f89348254f979
bayucr <bayfore@gmail.com>
2024-09-07 15:04:12 +0700

    07-09-2024

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
 
 const Grid = () => {
-  return <div>Grid</div>;
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map((item) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            img={item.img}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
 };
 
 export default Grid;
